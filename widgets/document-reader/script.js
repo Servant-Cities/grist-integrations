d092c939dd6a8532ae9d57d647c552e6b43fb7d9
Simon Mulquin <simon.mulquin@gmail.com>
2025-03-31 23:10:43 +0100

    feat(document-reader): ✨ Supports synchronous widgets

--- widgets/document-reader/script.js
+++ widgets/document-reader/script.js
@@ -1,18 +1,19 @@
+let currentWidget = "document-reader";
+
 // TODO: Use widget options instead of hardcoded constants
 const FORMAT_COLUMN = "Format";
 
 // Supported widgets
-const DOCUMENT_READER_URL = "http://localhost:4000/document-reader";
 const HTML_URL = "http://localhost:4000/styled-html";
-const DRAW_IO_URL =
-  "http://localhost:4000/draw-io-editor/?autosave=1&draw-io-url=https://embed.diagrams.net";
+const DRAW_IO_URL = "http://localhost:4000/draw-io-editor";
 
 const showError = message => {
   document.body.innerHTML = `<p style='color: red;'>${message}</p>`;
 };
 
-const loadWidget = async (url) => {
-  try {
+const renderWidget = async (url, record) => {
+  if (currentWidget !== url) {
+    currentWidget = url;
     const response = await fetch(url);
     if (!response.ok) throw new Error(`Failed to load widget from ${url}`);
 
@@ -21,38 +22,19 @@ const loadWidget = async (url) => {
     const fetchedDocument = parser.parseFromString(htmlContent, "text/html");
 
     document.body.innerHTML = "";
-    fetchedDocument.body.childNodes.forEach((node) => {
-      if (node.nodeName === "SCRIPT") {
-        const newScript = document.createElement("script");
-        newScript.src = node.src;
-        newScript.async = true;
-        document.head.appendChild(newScript);
-      } else {
-        document.body.appendChild(node.cloneNode(true));
-      }
+    fetchedDocument.body.childNodes.forEach(node => {
+      document.body.appendChild(node.cloneNode(true));
     });
 
-    document.head.innerHTML = "";
-    const rerouteScript = document.createElement("script");
-    rerouteScript.src = `${DOCUMENT_READER_URL}/script.js`;
-    rerouteScript.async = true;
-    document.head.appendChild(rerouteScript);
-    fetchedDocument.head.childNodes.forEach((node) => {
-      if (node.nodeName === "SCRIPT") {
-        const newScript = document.createElement("script");
-        newScript.src = node.src;
-        newScript.async = true;
-        document.head.appendChild(newScript);
-      } else {
-        document.head.appendChild(node.cloneNode(true));
-      }
-    });
-    
-  } catch (error) {
-    showError(error.message);
+    try {
+      const widget = await import(`${url}/script.js`);
+      widget.reset && widget.reset();
+      widget.onRecord && widget.onRecord(record);
+    } catch (error) {
+      document.body.innerHTML = `<p style='color: red;'>Failed to load widget-loader: ${error.message}</p>`;
+    }
   }
 };
-
 
 grist.onRecord(async record => {
   const existingErrorContainer = document.getElementById("error-container");
@@ -63,17 +45,13 @@ grist.onRecord(async record => {
   if (record) {
     switch (record[FORMAT_COLUMN]) {
       case "html":
-        loadWidget(HTML_URL);
+        renderWidget(HTML_URL, record);
         break;
       case "draw.io":
-        loadWidget(DRAW_IO_URL);
+        renderWidget(DRAW_IO_URL, record);
         break;
       default:
-        if (window.location.href === DOCUMENT_READER_URL) {
-          showError("Unsupported format");
-        } else {
-          window.location.href = DOCUMENT_READER_URL;
-        }
+        showError("Unsupported format");
     }
   }
 });
--- widgets/styled-html/script.js
+++ widgets/styled-html/script.js
@@ -1,18 +1,17 @@
-console.log('loading styled-html widget')
-
 // TODO: Use widget options instead of hardcoded constants
 const RAW_FILE_COLUMN = "Raw";
 
-grist.onRecord(async record => {
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = "";
+export const onRecord = async record => {
+  document.body.innerHTML = "";
 
   if (record && record[RAW_FILE_COLUMN]) {
     const rawHTML = record[RAW_FILE_COLUMN];
-    contentDiv.innerHTML = rawHTML;
+    document.body.innerHTML = rawHTML;
   } else {
-    contentDiv.innerHTML = `<div style="color: red; font-weight: bold;">Error: No valid content found.</div>`;
+    document.body.innerHTML = `<div style="color: red; font-weight: bold;">Error: No valid content found.</div>`;
   }
-});
+}
+
+grist.onRecord(onRecord);
 
 grist.ready();
