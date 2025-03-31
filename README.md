e63c627bd9d028c06790c287ecf92330fa70aec0
Simon Mulquin <simon.mulquin@gmail.com>
2025-03-31 23:24:27 +0100

    feat: 🚧 Trying to dynamically load asynchronous widgets

--- README.md
+++ README.md
@@ -10,14 +10,22 @@ yarn dev
 ```
 
 ## How to use the widgets in Grist ?
+The tools have yet to be made user friendly and documented here but they will work if you have the right setup:
+
 Add one of the following custom URLs
 
 ### Draw.io editor:
+Let you quickly read and write your draw.io documents and save them in Grist. (Soon)
+
 Autosave mode:
 http://localhost:4000/draw-io-editor/?autosave=1&draw-io-url=https://embed.diagrams.net
 
 Manual mode:
 http://localhost:4000/draw-io-editor/?draw-io-url=https://embed.diagrams.net
+
+### Styled html viewer:
+View your html files with css and scripts loaded
+http://localhost:4000/styled-html
 
 ## Host the widgets
 Build the server:
--- widgets/draw-io-editor/script.js
+++ widgets/draw-io-editor/script.js
@@ -1,27 +1,24 @@
-let selectedRecord;
 let baseUrl;
 let token;
-
-const iframe = document.getElementById("draw-io-editor");
+let iframeLoaded = false;
+let iframe;
+let selectedRecord;
 
 const params = new URLSearchParams(document.location.search);
-const drawIoUrl = params.get("draw-io-url");
+const drawIoUrl = params.get("draw-io-url") || "https://embed.diagrams.net";
 const autosave = params.get("autosave") || "0";
 const attachmentColumn = params.get("attachmentColumn") || "Attachment";
 
 const defaultXML = `<mxfile>
-  <diagram name="Page-1" id="H42mLJITaVdfGOS4gqnu">
+  <diagram name="Page-1">
     <mxGraphModel dx="1260" dy="781" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="827" pageHeight="1169" math="0" shadow="0">
       <root>
         <mxCell id="0" />
         <mxCell id="1" parent="0" />
-        <mxCell id="Hu7yD38lk9dKQbEFSdQO-3" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;" edge="1" parent="1" source="Hu7yD38lk9dKQbEFSdQO-1" target="Hu7yD38lk9dKQbEFSdQO-2">
-          <mxGeometry relative="1" as="geometry" />
-        </mxCell>
-        <mxCell id="Hu7yD38lk9dKQbEFSdQO-1" value="&lt;font style=&quot;color: rgb(255, 255, 255);&quot;&gt;&lt;b&gt;Start your diagram in Grist !&lt;/b&gt;&lt;/font&gt;" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#16B378;strokeColor=#16B378;" vertex="1" parent="1">
+        <mxCell id="start" value="Start your diagram in Grist" style="fillColor=#16B378;" vertex="1" parent="1">
           <mxGeometry x="304" y="150" width="220" height="90" as="geometry" />
         </mxCell>
-        <mxCell id="Hu7yD38lk9dKQbEFSdQO-2" value="&lt;font style=&quot;color: rgb(255, 255, 255);&quot;&gt;&lt;b&gt;But don&#39;t forget to save !&lt;/b&gt;&lt;/font&gt;" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#16B378;strokeColor=#16B378;" vertex="1" parent="1">
+        <mxCell id="end" value="Don't forget to save" style="fillColor=#16B378;" vertex="1" parent="1">
           <mxGeometry x="304" y="320" width="220" height="90" as="geometry" />
         </mxCell>
       </root>
@@ -29,8 +26,59 @@ const defaultXML = `<mxfile>
   </diagram>
 </mxfile>`;
 
-grist.onRecord(async record => {
+const handleMessage = async event => {
+  if (event.data && event.origin === drawIoUrl) {
+    try {
+      const message = JSON.parse(event.data);
+      switch (message.event) {
+        case "init":
+          console.log("Draw.io client is ready");
+
+          grist.ready({ requiredAccess: "full" });
+          onRecord(selectedRecord);
+          break;
+        case "save":
+        case "autosave":
+          console.log("Saving diagram");
+          saveFile(message.xml);
+          break;
+      }
+    } catch (err) {
+      console.error("Message parsing error", err);
+    }
+  }
+};
+
+if (!window.drawIoListenerAttached) {
+  window.addEventListener("message", handleMessage);
+  window.drawIoListenerAttached = true;
+}
+
+export const reset = record => {
+  baseUrl = null;
+  token = null;
+  iframeLoaded = false;
+  iframe = document.getElementById("draw-io-editor");
+
+  if (!iframe) {
+    console.error("Iframe element not found");
+    return;
+  }
+
+  iframe.src = `${drawIoUrl}?embed=1&proto=json&spin=1`;
+
+  iframe.onload = () => {
+    iframeLoaded = true;
+    onRecord(record);
+    console.log("Iframe loaded");
+  };
+
+  console.log("Reset complete");
+};
+
+export const onRecord = async record => {
   if (record) {
+    selectedRecord = record;
     if (!baseUrl || !token) {
       const access = await grist.getAccessToken();
       baseUrl = access.baseUrl;
@@ -62,36 +110,7 @@ grist.onRecord(async record => {
       );
     }
   }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  iframe.src = `${drawIoUrl}?embed=1&proto=json&spin=1`;
-
-  window.addEventListener("message", async event => {
-    if (event.data && event.origin === drawIoUrl) {
-      try {
-        const message = JSON.parse(event.data);
-        switch (message.event) {
-          case "init":
-            console.log("The draw.io client is ready");
-
-            grist.ready({
-              requiredAccess: "full",
-            });
-
-            break;
-          case "save":
-          case "autosave":
-            console.log(message);
-            saveFile(message.xml);
-            break;
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  });
-});
+};
 
 const saveFile = async xml => {
   const table = grist.selectedTable;
