11a494ce3952950d5208fe3e5d82476434df754f
Simon Mulquin <simon.mulquin@gmail.com>
2025-04-05 17:08:58 +0100

    fix(og-viewer): 🐛 Proper fallback when OG protocol is compromised

--- host-grist-widget/src/server.ts
+++ host-grist-widget/src/server.ts
@@ -5,14 +5,12 @@ import fastifyStatic from "@fastify/static";
 
 const fastify = Fastify();
 
-// Enable CORS for all origins
 fastify.register(fastifyCors, {
   origin: "*",
 });
 
-const MAX_REDIRECTS = 5; // Maximum number of redirects to follow
+const MAX_REDIRECTS = 5;
 
-// Function to fetch a URL and handle redirects
 async function fetchWithRedirects(url: string, redirects = 0): Promise<string> {
   if (redirects >= MAX_REDIRECTS) {
     throw new Error('Too many redirects');
@@ -25,41 +23,39 @@ async function fetchWithRedirects(url: string, redirects = 0): Promise<string> {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
       },
-      redirect: "manual", // Do not follow redirects automatically
+      redirect: "manual",
     });
 
     const contentType = response.headers.get("Content-Type") || "";
-    // If it's an HTML page, check for redirects
     if (response.status >= 300 && response.status < 400) {
       const redirectUrl = response.headers.get("Location");
+      console.log("redirect ", url, "to : ", redirectUrl)
       if (redirectUrl) {
-        // Follow the redirect URL
         return fetchWithRedirects(redirectUrl, redirects + 1);
       } else {
         throw new Error('Redirect URL missing');
       }
     }
 
-    // Return the body as text if it's not a redirect
     if (contentType.includes("text/html")) {
       return await response.text();
     } else {
-      return url; // If the content is not HTML, return the URL as is
+      return url;
     }
   } catch (error) {
     throw new Error(`Error fetching URL: ${error.message}`);
   }
 }
 
-// Proxy route to handle fetching URLs
 fastify.get("/proxy", async (request, reply) => {
   const { url } = request.query as { url: string };
+  console.log("proxy requested for : ", url)
   if (!url) {
     return reply.code(400).send("URL parameter is required");
   }
 
   try {
-    const htmlContent = await fetchWithRedirects(url); // Fetch the content, handling redirects
+    const htmlContent = await fetchWithRedirects(url);
     reply.type("text/html").send(htmlContent);
   } catch (error) {
     console.error("Error:", error.message);
@@ -67,7 +63,6 @@ fastify.get("/proxy", async (request, reply) => {
   }
 });
 
-// Static file serving (for widgets, etc.)
 const widgetsDir = path.resolve("../widgets");
 
 fastify.register(fastifyStatic, {
@@ -75,7 +70,6 @@ fastify.register(fastifyStatic, {
   prefix: "/",
 });
 
-// Start the server
 const start = async () => {
   try {
     await fastify.listen({ port: 4000, host: "0.0.0.0" });
