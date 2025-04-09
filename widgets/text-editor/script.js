81c6abe7b3b6482ef342b1e99fb95cf9888c9d59
Simon Mulquin <simon.mulquin@gmail.com>
2025-04-09 11:29:41 +0100

    fix(document-reader): 🐛 Non iframe widgets blink

--- widgets/text-editor/script.js
+++ widgets/text-editor/script.js
@@ -4,12 +4,14 @@ const params = new URLSearchParams(document.location.search);
 const rawColumn = params.get("rawColumn") || "Raw";
 
 export const reset = record => {
+  console.log("reset !");
   editor = document.getElementById("editor");
+
+  editor.value = record[rawColumn] || "";
+
   if (editor) {
-    editor.value = record[rawColumn] || "";
     editor.addEventListener("input", async () => {
       if (!selectedRecord) return;
-      const table = grist.selectedTable;
       await table.update({
         id: selectedRecord.id,
         fields: {
@@ -22,8 +24,10 @@ export const reset = record => {
 };
 
 export const onRecord = async record => {
-  selectedRecord = record;
-  if (editor) {
+  console.log("onRecord !", editor);
+  if (editor && editor.value !== record[rawColumn]) {
+    console.log("onRecord changed !", record[rawColumn]);
+    selectedRecord = record;
     editor.value = record[rawColumn] || "";
   }
 };
